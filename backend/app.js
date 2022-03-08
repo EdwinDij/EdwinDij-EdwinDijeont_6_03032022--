3b6e84d9fde3c14a0e4ba4af8ce621828756aa96
Edwin <edwin.d899@gmail.com>
2022-03-08 18:20:36 +0100

    création des route post

--- backend/app.js
+++ backend/app.js
@@ -2,23 +2,37 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('Requête reçue !');
+app.use(express.json());
+
+app.use((req, res, next)=> {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+app.post
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
+app.use('/api/stuff', (req, res, next) => {
 
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
-
+  const stuff =[
+    {
+      _id: 'oeihfzeoi',
+      title: 'Mon premier objet',
+      description: 'Les infos de mon premier objet',
+      imageUrl: '',
+      price: 4900,
+      userId: 'gsomihvgios',
+    },
+    {
+      _id: 'oeihfzeomoihi',
+      title: 'Mon deuxième objet',
+      description: 'Les infos de mon premier objet',
+      imageUrl: '',
+      price: 2900,
+      userId: 'gsomihvgios',
+    },
+  ];
+res.status(200).json(stuff)
+})
 module.exports = app;
