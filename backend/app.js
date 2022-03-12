21a488ca61df8bd6c0d941499de3af439ba6e36e
Edwin <edwin.d899@gmail.com>
2022-03-12 21:11:33 +0100

    ajout de helmet , rate-limit, mongosanitize pour plus de sécurité dans l'api

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,12 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 
+const rateLimit = require('express-rate-limit')
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+
+app.use(helmet());
+app.use(mongoSanitize());
 
 require('dotenv').config()
 const db = process.env;
@@ -16,6 +22,7 @@ mongoose.connect('mongodb+srv://EdwinAdm:PbveaZ5VCOY7kkB9@piiquante.6sa2s.mongod
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+
 const app = express();
 
 app.use((req, res, next) => {
@@ -25,7 +32,33 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(
+    mongoSanitize({
+      allowDots: true,
+    }),
+  );
+
+  app.use(
+    mongoSanitize({
+      allowDots: true,
+      replaceWith: '_',
+    }),
+  );
+
+  import rateLimit from 'express-rate-limit'
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter)
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
